fecf6e5a02ce29ceb3ab7db43cef699bb7bdfdb4
Creepercdn <creepercdn@outlook.com>
2022-08-31 22:12:43 +0800

    MIDIDevice and timer

---
+++ include/libmfmidi/abstractmididevice.hpp
@@ -0,0 +1,60 @@
+/*
+ * This file is a part of libmfmidi.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/// \file abstractmididevice.hpp
+/// \author Creepercdn (creepercdn@outlook.com)
+/// \brief MIDIDriver
+
+#pragma once
+
+#include "midimessage.hpp"
+#include <functional>
+#include <utility>
+
+namespace libmfmidi {
+    /// \brief A class that abstracted MIDI device.
+    class AbstractMIDIDevice {
+    public:
+        AbstractMIDIDevice() noexcept = default;
+
+        AbstractMIDIDevice(const AbstractMIDIDevice&)            = default;
+        AbstractMIDIDevice(AbstractMIDIDevice&&)                 = default;
+        AbstractMIDIDevice& operator=(const AbstractMIDIDevice&) = default;
+        AbstractMIDIDevice& operator=(AbstractMIDIDevice&&)      = default;
+        using callback_type                                      = std::function<void(const MIDIMessage& msg)>;
+
+        virtual ~AbstractMIDIDevice() noexcept = default;
+
+        [[nodiscard]] virtual bool isOpen() const noexcept = 0;
+
+        [[nodiscard]] virtual constexpr bool inputAvailable() const noexcept = 0;
+        [[nodiscard]] virtual constexpr bool outputAvailable() const noexcept = 0;
+
+        virtual bool open() = 0;
+        virtual bool stop() = 0;
+
+        virtual void sendMsg(const MIDIMessage& msg) = 0;
+
+        virtual void setCallback(callback_type usercb) noexcept
+        {
+            mcb = std::move(usercb);
+        }
+
+    protected:
+        callback_type mcb;
+    };
+}
---
+++ include/libmfmidi/abstracttimer.hpp
@@ -0,0 +1,64 @@
+/*
+* This file is a part of libmfmidi.
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// \file abstracttimer.hpp
+/// \author Creepercdn (creepercdn@outlook.com)
+/// \brief Timer
+
+#pragma once
+
+#include <functional>
+#include <utility>
+
+namespace libmfmidi::details {
+    class Win32MMTimer;
+    class POSIXTimer;
+}
+
+namespace libmfmidi {
+    /// \brief Abstract timer
+    class AbstractTimer {
+    public:
+        virtual ~AbstractTimer() noexcept = default;
+        using callback_type = std::function<void()>;
+        virtual bool start(unsigned int delay) = 0;
+
+        virtual bool stop() = 0;
+
+        virtual void setCallback(callback_type func) noexcept
+        {
+            m_cb = std::move(func);
+        }
+
+        [[nodiscard]] virtual int delay() const noexcept
+        {
+            return mdelay;
+        }
+
+    protected:
+        callback_type m_cb;
+        bool          ison = false;
+        unsigned int mdelay = 0;
+    };
+
+#if defined(_WIN32)
+    using PlatformTimer = details::Win32MMTimer;
+#elif defined(_POSIX_)
+    using PlatformTimer = details::POSIXTimer;
+#endif
+
+}
