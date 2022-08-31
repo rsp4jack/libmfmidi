afae4b4c6589fda5ef2da03d8d72960096dd3964
Creepercdn <creepercdn@outlook.com>
2022-08-31 22:11:18 +0800

    Timer and RtmidiDevice

---
+++ include/libmfmidi/details/rtmididevice.hpp
@@ -0,0 +1,211 @@
+/*
+ * This file is a part of libmfmidi.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/// \file rtmididevice.hpp
+/// \author Creepercdn (creepercdn@outlook.com)
+/// \brief RtMidi MIDIDevice support
+
+#pragma once
+
+#include "rtmidi/RtMidi.h"
+#include "abstractmididevice.hpp"
+#include <string>
+#include <utility>
+#include <unordered_set>
+#include <memory>
+
+namespace libmfmidi::details {
+    class RtMidiInDevice : public AbstractMIDIDevice {
+    public:
+        explicit RtMidiInDevice(unsigned int id, std::string name = "libmfmidi RtMidiInDevice") noexcept
+            : mid(id)
+            , mnm(std::move(name))
+        {
+            initiazle();
+        }
+
+        explicit RtMidiInDevice(std::string virtualPortName) noexcept
+            : mnm(std::move(virtualPortName))
+            , mvirtual(true)
+        {
+            initiazle();
+        }
+
+        ~RtMidiInDevice() noexcept override = default;
+
+        RtMidiInDevice(RtMidiInDevice&&) noexcept        = default;
+        RtMidiInDevice(const RtMidiInDevice&)            = delete;
+        RtMidiInDevice& operator=(const RtMidiInDevice&) = delete;
+        RtMidiInDevice& operator=(RtMidiInDevice&&)      = delete;
+
+        bool open() override
+        {
+            if (mvirtual) {
+                min.openVirtualPort(mnm);
+            } else {
+                min.openPort(mid, mnm);
+            }
+            return min.isPortOpen();
+        }
+
+        bool stop() override
+        {
+            min.closePort();
+            return !min.isPortOpen();
+        }
+
+        [[nodiscard]] bool isOpen() const noexcept override
+        {
+            return min.isPortOpen();
+        }
+
+        [[nodiscard]] constexpr bool inputAvailable() const noexcept override
+        {
+            return true;
+        }
+
+        [[nodiscard]] constexpr bool outputAvailable() const noexcept override
+        {
+            return false;
+        }
+
+        void sendMsg(const MIDIMessage& msg) override {}
+
+    private:
+        static void rtcallback(double /*timeStamp*/, std::vector<unsigned char>* message, void* userData)
+        {
+            reinterpret_cast<RtMidiInDevice*>(userData)->mcb(MIDIMessage(*message));
+        }
+
+        void initiazle() noexcept
+        {
+            min.setCallback(rtcallback, this);
+        }
+
+        RtMidiIn     min;
+        unsigned int mid{};
+        std::string  mnm;
+        bool         mvirtual = false;
+    };
+
+    class RtMidiOutDevice : public AbstractMIDIDevice {
+    public:
+        explicit RtMidiOutDevice(unsigned int id, std::string name = "libmfmidi RtMidiOutDevice") noexcept
+            : mid(id)
+            , mnm(std::move(name))
+        {
+        }
+
+        explicit RtMidiOutDevice(std::string virtualPortName) noexcept
+            : mnm(std::move(virtualPortName))
+            , mvirtual(true)
+        {
+        }
+
+        ~RtMidiOutDevice() noexcept override = default;
+        RtMidiOutDevice(const RtMidiOutDevice& rhs) noexcept        = delete;
+        RtMidiOutDevice(RtMidiOutDevice&&) noexcept                 = default;
+        RtMidiOutDevice& operator=(const RtMidiOutDevice&) noexcept = delete;
+        RtMidiOutDevice& operator=(RtMidiOutDevice&&) noexcept      = delete;
+
+        bool open() override
+        {
+            if (mvirtual) {
+                min.openVirtualPort(mnm);
+            } else {
+                min.openPort(mid, mnm);
+            }
+            return min.isPortOpen();
+        }
+
+        bool stop() override
+        {
+            min.closePort();
+            return !min.isPortOpen();
+        }
+
+        [[nodiscard]] bool isOpen() const noexcept override
+        {
+            return min.isPortOpen();
+        }
+
+        [[nodiscard]] constexpr bool inputAvailable() const noexcept override
+        {
+            return false;
+        }
+
+        [[nodiscard]] constexpr bool outputAvailable() const noexcept override
+        {
+            return true;
+        }
+
+        void sendMsg(const MIDIMessage& msg) override
+        {
+            min.sendMessage(&msg.data());
+        }
+
+    private:
+        RtMidiOut    min;
+        unsigned int mid{};
+        std::string  mnm;
+        bool         mvirtual = false;
+    };
+
+    class RtMidiMIDIDeviceProvider {
+    public:
+        unsigned int inputCount()
+        {
+            return auxin.getPortCount();
+        }
+
+        unsigned int outputCount()
+        {
+            return auxout.getPortCount();
+        }
+
+        std::string inputName(unsigned int idx)
+        {
+            return auxin.getPortName(idx);
+        }
+
+        std::string outputName(unsigned int idx)
+        {
+            return auxout.getPortName(idx);
+        }
+
+        static std::shared_ptr<RtMidiInDevice> buildupInputDevice(unsigned int idx, const std::string& name = "libmfmidi RtMidiMIDIDeviceProvider IN")
+        {
+            return std::make_shared<RtMidiInDevice>(idx, name);
+        }
+
+        static std::shared_ptr<RtMidiOutDevice> buildupOutputDevice(unsigned int idx, const std::string& name = "libmfmidi RtMidiMIDIDeviceProvider OUT"){
+            return std::make_shared<RtMidiOutDevice>(idx, name);
+        }
+
+    private:
+        RtMidiMIDIDeviceProvider() noexcept = default;
+
+        RtMidiIn  auxin;  // for device info
+        RtMidiOut auxout; // too
+
+        static RtMidiMIDIDeviceProvider& instance() noexcept
+        {
+            static RtMidiMIDIDeviceProvider ins; // Meyers' Singleton
+            return ins;
+        }
+    };
+}
---
+++ include/libmfmidi/details/win32mmtimer.hpp
@@ -0,0 +1,91 @@
+/*
+ * This file is a part of libmfmidi.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/// \file win32mmtimer.hpp
+/// \author Creepercdn (creepercdn@outlook.com)
+/// \brief Win32 Multimedia Timer
+
+#pragma once
+
+#if defined(_WIN32)
+#include <Windows.h>
+#include <timeapi.h>
+#include <mmsystem.h>
+#include "abstracttimer.hpp"
+#include <stdexcept>
+#include <algorithm>
+#include <string>
+
+namespace libmfmidi::details {
+    class Win32MMTimer : public AbstractTimer {
+    public:
+        static constexpr unsigned int resolution = 5;
+
+        ~Win32MMTimer() noexcept override
+        {
+            stop();
+        }
+
+        bool start(unsigned int delay) override
+        {
+            if (ison) {
+                return true;
+            }
+            TIMECAPS caps;
+            auto     val = timeGetDevCaps(&caps, sizeof(TIMECAPS)); // reserved
+            if (val != MMSYSERR_NOERROR) {
+                return false;
+            }
+            mdelay = std::clamp(delay, caps.wPeriodMin, caps.wPeriodMax);
+            if (timeBeginPeriod(mdelay) != TIMERR_NOERROR) {
+                return false;
+            }
+            ison = true;
+            timerid = timeSetEvent(mdelay, resolution, win32cb, reinterpret_cast<DWORD_PTR>(this), TIME_PERIODIC | TIME_KILL_SYNCHRONOUS);
+            if (!timerid) {
+                ison = false;
+                timeEndPeriod(mdelay);
+                return false;
+            }
+            return true;
+        }
+
+        bool stop() override
+        {
+            if (!ison) {
+                return true;
+            }
+            if (timeKillEvent(timerid) != TIMERR_NOERROR) {
+                return false;
+            }
+            if (timeEndPeriod(mdelay) != TIMERR_NOERROR) {
+                return false;
+            }
+            return true;
+        }
+
+    private:
+        static void CALLBACK win32cb(UINT  /*uTimerID*/, UINT  /*uMsg*/, DWORD_PTR dwUser, DWORD_PTR  /*dw1*/, DWORD_PTR  /*dw2*/)
+        {
+            reinterpret_cast<Win32MMTimer*>(dwUser)->m_cb();
+        }
+
+        unsigned int timerid;
+    };
+}
+
+#endif
