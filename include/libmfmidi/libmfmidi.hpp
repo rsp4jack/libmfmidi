2d943bdd26e4aafd42f27791f4560519612c6aa4
Creepercdn <creepercdn@outlook.com>
2022-08-31 22:12:19 +0800

    arch test in libmfmidi.hpp

--- include/libmfmidi/libmfmidi.hpp
+++ include/libmfmidi/libmfmidi.hpp
@@ -2,6 +2,9 @@
 /// \author Creepercdn (creepercdn@outlook.com)
 /// \brief Main file of libmfmidi
 
+#pragma once
+#include <climits>
+
 /*!
  * \mainpage
  *
@@ -9,8 +12,9 @@
 
    \section design Design
 
-   Channel is begin from 1. (except internal)
+   Channel is begin from 1.
    Tracks is begin from 0.
+   Ports is begin from 1.
 
  * \section install Installation
  *
@@ -18,4 +22,20 @@
 
  * \section faq Frequently Asked Questions
     No one asked me...
- */
+*/
+
+namespace libmfmidi {
+    namespace {
+        inline void _pseudo_check_arch() noexcept
+        {
+#if !defined(__LLP64__) && !defined(__ILP32__)
+            static_assert(CHAR_BIT == 8);
+            static_assert(sizeof(char) == 1); // of course.
+            static_assert(sizeof(short int) == 2);
+            static_assert(sizeof(int) == 4);
+            static_assert(sizeof(long int) == 4); // FIXME: LP64 (*nix)
+            static_assert(sizeof(long long int) == 8);
+#endif
+        }
+    }
+}
