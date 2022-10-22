3914c85a3fe3d48380fe9992ee94425eee879d6c
Creepercdn <creepercdn@outlook.com>
2022-10-22 10:40:39 +0800

    trackplayer and example

---
+++ example/MTTrackPlayer.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <fstream>
+#include "libmfmidi/simpletrackplayer.hpp"
+#include "libmfmidi/smfreader.hpp"
+#include "libmfmidi/samhandlers.hpp"
+#include "libmfmidi/details/rtmididevice.hpp"
+#include "libmfmidi/details/kdmapidevice.hpp"
+#include "libmfmidi/details/win32mmtimer.hpp"
+#include "libmfmidi/midimessagefdc.hpp"
+
+using namespace libmfmidi;
+using std::cout;
+using std::endl;
+
+int main(int argc, char** argv)
+{
+    cout << "MTTrackPlayer: Example of libmfmidi" << endl;
+    if (argc == 1) {
+        std::cerr << "Error: No input file" << std::endl;
+        return -1;
+    }
+
+    cout << "Opening file " << argv[1] << endl;
+    std::fstream stm;
+    char filebuffer[2048]{};
+    stm.rdbuf()->pubsetbuf(filebuffer, 2048);
+
+    stm.open(argv[1], std::ios::in | std::ios::binary);
+    cout << "Opened" << endl;
+
+    MIDIMultiTrack    file;
+    SMFFileInfo       info;
+    SMFFileSAMHandler hsam(&file, &info);
+    SMFReader         rd(&stm, 0, &hsam);
+
+    cout << "Parsing SMF" << endl;
+    rd.parse();
+    cout << "Parsed" << endl;
+    cout << "SMF File: Format " << info.type << "; Division: " << static_cast<uint16_t>(info.division) << ";" << endl;
+    cout << "NTrks: " << file.size() << ';' << endl;
+
+    stm.close();
+
+    cout << "Merging" << endl;
+    MIDITrack trk;
+    mergeMultiTrack(std::move(file), trk);
+    cout << "Merged" << endl;
+
+    details::RtMidiMIDIDeviceProvider& prov = details::RtMidiMIDIDeviceProvider::instance();
+    cout << "Dev cnt: " << prov.outputCount() << endl;
+    for (unsigned int i = 0; i < prov.outputCount(); ++i) {
+        cout << prov.outputName(i) << endl;
+    }
+    cout << "Choose, " << prov.outputCount() + 1 << " to KDMAPI: ";
+    unsigned inp;
+    std::cin >> inp;
+
+    AbstractMIDIDevice*                                  dev;
+    std::unique_ptr<libmfmidi::details::KDMAPIDevice>    kdev;
+    std::shared_ptr<libmfmidi::details::RtMidiOutDevice> sdev;
+    if (inp == prov.outputCount() + 1) {
+        kdev = std::make_unique<libmfmidi::details::KDMAPIDevice>(true);
+        dev  = kdev.get();
+    } else {
+        sdev = libmfmidi::details::RtMidiMIDIDeviceProvider::buildupOutputDevice(inp);
+        dev  = sdev.get();
+    }
+
+    if (!dev->open()) {
+        std::cerr << "Failed to open device" << endl;
+    }
+
+    MIDIMessageF2D        f2d;
+    details::Win32MMTimer timer;
+    SimpleTrackPlayer     player; // init player after everything
+
+    player.setMsgProcessor(f2d);
+    player.setDivision(info.division);
+    player.setDriver(dev);
+    player.setTimer(&timer);
+
+    cout << "Preprocessing" << endl;
+    player.setTrack(std::move(trk));
+    cout << "Preprocessed" << endl;
+
+    cout << "Calling playing" << endl;
+    player.play();
+    cout << "Playing, Press a key to quit" << endl;
+    std::system("pause");
+    player.pause();
+    return 0;
+}
---
+++ include/libmfmidi/simpletrackplayer.hpp
@@ -0,0 +1,157 @@
+/*
+ * This file is a part of libmfmidi.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/// \file SimpleTrackPlayer.hpp
+/// \author Creepercdn (creepercdn@outlook.com)
+/// \brief A very simple multithreaded MIDITrack player
+
+#pragma once
+
+#include "mfconcepts.hpp"
+#include "miditrack.hpp"
+#include <functional>
+#include "abstractmididevice.hpp"
+#include "abstracttimer.hpp"
+
+// Together for a std::shared_future!
+
+namespace libmfmidi {
+    class SimpleTrackPlayer {
+    public:
+        static constexpr unsigned int PLAYER_RESOLUTION = 1; // 1ms
+        void play()
+        {
+            mtimer->start(PLAYER_RESOLUTION);
+            mplay = true;
+        }
+
+        void pause()
+        {
+            if(mtimer != nullptr){
+                mtimer->stop();
+            }
+            mplay = false;
+        }
+
+        void goZero()
+        {
+            pause();
+            cur      = mtrk.begin();
+            mreltimertick = 0;
+        }
+
+        void setTimer(AbstractTimer* timer)
+        {
+            pause();
+            mtimer = timer;
+            timer->setCallback([this] { _timertick(); });
+        }
+
+        bool isPlaying() const noexcept
+        {
+            return mplay;
+        }
+
+        template <MIDIProcessor T>
+        void setMsgProcessor(T& processor) noexcept
+        {
+            mprocess = [ObjectPtr = &processor](auto&& PH1) { return ObjectPtr->process(std::forward<decltype(PH1)>(PH1)); };
+        }
+
+        void setDivision(MIDIDivision divi)
+        {
+            division = divi;
+            updateTickTime();
+        }
+
+        void setTrack(MIDITrack&& trk)
+        {
+            mtrk = std::move(trk);
+            // pre processing
+            auto it = mtrk.begin();
+            while (it != mtrk.end()) {
+                if (!mprocess(*it)) {
+                    auto tmp = it - mtrk.begin();
+                    if (mtrk.end() - it > 1) {
+                        (it + 1)->setDeltaTime(it->deltaTime() + (it + 1)->deltaTime());
+                    }
+                    mtrk.erase(it);
+                    it = mtrk.begin() + tmp;
+                    continue;
+                }
+                ++it;
+            }
+            cur = mtrk.begin();
+        }
+
+        void setDriver(AbstractMIDIDevice* adrv)
+        {
+            pause();
+            if(adrv->outputAvailable()){
+                drv = adrv;
+            }
+        }
+
+        void setTempo(MIDITempo bpm)
+        {
+            tempobpm = bpm;
+            updateTickTime();
+        }
+
+    private:
+        AbstractMIDIDevice*   drv;
+        AbstractTimer*        mtimer{};
+        MIDITrack             mtrk;
+        MIDITrack::iterator   cur;
+        MIDIClockTime         mreltimertick = 0;
+        bool                  mplay    = false;
+        MIDITempo              tempobpm = 120;
+        MIDIDivision          division = 96;
+        MIDIProcessorFunction mprocess;
+        double                 tickTime = divisionToSec(division, tempobpm) * 1000; // cache: milliseconds per midi tick
+
+        void updateTickTime() noexcept
+        {
+            tickTime = divisionToSec(division, tempobpm)*1000;
+        }
+
+        void _timertick()
+        {
+            // About this loop:
+            // For repeated 0 or low deltatime events
+            // i think this will not work, because timer tick will overstock when callback spend too long time (> timer delay)
+            // but it works
+            while(true){
+                if (cur >= mtrk.end()) {
+                    pause();
+                    return;
+                }
+                if (mreltimertick * PLAYER_RESOLUTION < tickTime * cur->deltaTime()) {
+                    ++mreltimertick;
+                    return;
+                }
+                if (cur->MFMarker() == MFMessageMark::Tempo) {
+                    setTempo(rawCat(cur->byte0(), cur->byte1(), cur->byte2(), cur->byte3()));
+                } else {
+                    drv->sendMsg(*cur);
+                }
+                ++cur;
+                mreltimertick = 0; // when next tick come it past 1 tick, but there is a loop so no time between next tick
+            }
+        }
+    };
+}
